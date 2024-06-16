04f47d581187f081396bf9c1d36fce1210eeb387
Paolo <paolomabale@gmail.com>
2024-06-16 14:29:37 +0800

    functional RPS game

--- bato-pick.js
+++ bato-pick.js
@@ -4,7 +4,8 @@
 let computerSelection = getComputerChoice();
 
 // asks for user input using prompt(), turns playerSelection case insensitive
-let playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+// let playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+let playerSelection = getHumanChoice();
 
 // score counters started as 0
 let playerScore = 0;
@@ -13,7 +14,6 @@ let tie = 0;
 
 // game rounds 
 let gameRound = 0;
-let playerSelection = getHumanChoice();
 
 
 function getHumanChoice() {
@@ -35,17 +35,6 @@ function getComputerChoice() {
 // SOLUTION 1: Added score increment++ to the function along with game condition win/lose/tie
 // compare player/computerSelection, plays 1 round of RPS
 // increments score counters with game results (user and pc wins and ties)
-
-function playGame() {
-
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection === computerSelection) {
-            tie++;
-            return "Neither Wins it's a TIE."
-        } else if (
-            playerSelection === 'rock' && computerSelection === 'scissors' ||
-            playerSelection === 'paper' && computerSelection === 'rock' ||
-            playerSelection === 'scissors' && computerSelection === 'paper'
 
 function playGame() {
 
@@ -94,16 +83,17 @@ function playGame() {
         // updates number of rounds and scores
         gameRound++;
     }
-    // prints scores to console
+
+    // prints scores and game round to connsole
     console.log("player: " + playerSelection);
     console.log("computer: " + computerSelection)
     console.log("Game Round: " + gameRound);
     console.log("Player Score:" + playerScore);
     console.log("Computer Score:" + computerScore);
     console.log("Tie:" + tie);
-    // and store game data such as scores and number of roundsq
 }
-// play 1 round of RPS
+
+// play 5 rounds of RPS
 playGame();
 
 // PROBLEM 2: faced problem of score not updating
